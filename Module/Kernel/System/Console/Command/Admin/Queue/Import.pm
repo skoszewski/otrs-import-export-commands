8c717ebf473e655c63251bd68909a8688f263b5e
Sławomir Koszewski <slawek@koszewscy.waw.pl>
2016-10-06 09:57:13 +0200

    Reengineered Queue::Import command and bumped module version.

--- Module/Kernel/System/Console/Command/Admin/Queue/Import.pm
+++ Module/Kernel/System/Console/Command/Admin/Queue/Import.pm
@@ -11,200 +11,175 @@ package Kernel::System::Console::Command::Admin::Queue::Import;
 use strict;
 use warnings;
 
-use Text::CSV;
-
-use base qw(Kernel::System::Console::BaseCommand);
+use base qw(Kernel::System::Console::ImportCommand);
 
 our @ObjectDependencies = (
     'Kernel::System::Queue',
-    'Kernel::System::Log',
     'Kernel::System::Valid',
     'Kernel::System::Group',
     'Kernel::System::SystemAddress',
     'Kernel::System::Salutation',
     'Kernel::System::Signature',
+    'Kernel::System::DB',
 );
 
 sub Configure {
     my ( $Self, %Param ) = @_;
 
-    $Self->Description('Imports several queues from a CSV file.');
+    $Self->{ObjectClass} = 'Kernel::System::Queue';
+    $Self->{CacheType} = "Queue";
+    $Self->{PropertyNames} = [
+        "Name",
+        "ValidID",
+        "GroupID",
+        "Calendar",
+        "FirstResponseTime",
+        "UpdateTime",
+        "SolutionTime",
+        "UnlockTimeout",
+        "SystemAddressID",
+        "SalutationID",
+        "SignatureID",
+        "FollowUpID",
+        "Comment",
+    ];
 
-    $Self->AddArgument(
-        Name        => 'file',
-        Description => "A CSV formatted input file with queue definitions.",
-        Required    => 1,
-        ValueRegex  => qr/.*\.csv$/smxi,
+    $Self->{ObjectIdName} = "QueueID";  # Custom ID name
+
+    $Self->SUPER::Configure();
+
+    # create a hash with follow-up option names
+    my $DBObject = $Kernel::OM->Get('Kernel::System::DB');
+
+    return if !$DBObject->Prepare(
+        SQL => 'SELECT id, name FROM follow_up_possible',
     );
+    
+    my %FollowUpOptionsList;
+
+    while ( my @Row = $DBObject->FetchrowArray() ) {
+        $FollowUpOptionsList{ $Row[0] } = $Row[1];
+    }
+
+    my %ReversedFollowUpOptionsList = reverse %FollowUpOptionsList;
+    $Self->{FollowUpOptionsList} = \%ReversedFollowUpOptionsList;
+
+    # reversed queue list
+    my %ReversedQueueList = reverse $Self->{DataObject}->QueueList( Valid => 0 );
+    $Self->{ObjectList} = \%ReversedQueueList;
 
     return;
 }
 
-sub PreRun {
-    my ( $Self, %Param ) = @_;
+sub ObjectGet {
+    my ( $Self, $ObjectId ) = @_;
 
-    # Check if the specified file exists.
-    my $FileName = $Self->GetArgument('file');
+    return $Self->{DataObject}->QueueGet(
+        ID => $ObjectId
+    );
+}
 
-    if ( ! -e $FileName ) {
-        # No? Abort then.
-        die "File $FileName not found.\n";
+sub ObjectProperty {
+    my ( $Self, $ColumnName, $ColumnText ) = @_;
+
+    # Name, ValidID, GroupID, Calendar, FirstResponseTime, UpdateTime, SolutionTime, UnlockTimeout, SystemAddressID, SalutationID, SignatureID, FollowUpID, Comment
+
+    if ( $ColumnName =~ m/^name$/i ) {                          # Name
+        return ( 'Name', $ColumnText );
+    } elsif ( $ColumnName =~ m/^valid$/i ) {                    # Valid
+
+        my %ValidList = reverse $Kernel::OM->Get('Kernel::System::Valid')->ValidList(); 
+        my $ValidID = $ValidList{$ColumnText};
+        if (!$ValidID) {
+            $Self->Print("<red>\"$ColumnText\" is not a valid name.</red>\n");
+        }
+        return ( 'ValidID', $ValidID || 1 );
+
+    } elsif ( $ColumnName =~ m/^group$/i ) {                    # Group
+
+        my %GroupList = reverse $Kernel::OM->Get('Kernel::System::Group')->GroupList();
+        my $GroupID = $GroupList{$ColumnText};
+        if (!$GroupID) {
+            $Self->Print("<red>\"$ColumnText\" is not a valid Group name.</red>\n");
+        }
+        return ( 'GroupID', $GroupID || 1 );
+
+    } elsif ( $ColumnName =~ m/^calendar$/i ) {                 # Calendar
+
+        return ( 'Calendar', $ColumnText || '' );
+
+    } elsif ( $ColumnName =~ m/^first *response *time$/i ) {    # First Response Time
+
+        return ( 'FirstResponseTime', $ColumnText || '' );
+
+    } elsif ( $ColumnName =~ m/^update *time$/i ) {             # Update Time
+
+        return ( 'UpdateTime', $ColumnText || '' );
+
+    } elsif ( $ColumnName =~ m/^solution *time$/i ) {           # Solution Time
+
+        return ( 'SolutionTime', $ColumnText || '' );
+
+    } elsif ( $ColumnName =~ m/^unlock *timeout$/i ) {          # Unlock Timeout
+
+        return ( 'UnlockTimeout', $ColumnText || '' );
+
+    } elsif ( $ColumnName =~ m/^system *address$/i ) {          # SystemAddress
+
+        my %SystemAddressList = reverse $Kernel::OM->Get('Kernel::System::SystemAddress')->SystemAddressList();
+        my $SystemAddressID = $SystemAddressList{$ColumnText};
+        if (!$SystemAddressID) {
+            $Self->Print("<red>\"$ColumnText\" is not a valid SystemAddress name.</red>\n");
+        }
+        return ( 'SystemAddressID', $SystemAddressID || 1 );
+
+    } elsif ( $ColumnName =~ m/^salutation$/i ) {               # Salutation
+
+        my %SalutationList = reverse $Kernel::OM->Get('Kernel::System::Salutation')->SalutationList();
+        my $SalutationID = $SalutationList{$ColumnText};
+        if (!$SalutationID) {
+            $Self->Print("<red>\"$ColumnText\" is not a valid Salutation name.</red>\n");
+        }
+        return ( 'SalutationID', $SalutationID || 1 );
+
+    } elsif ( $ColumnName =~ m/^signature$/i ) {                # Signature
+
+        my %SignatureList = reverse $Kernel::OM->Get('Kernel::System::Signature')->SignatureList();
+        my $SignatureID = $SignatureList{$ColumnText};
+        if (!$SignatureID) {
+            $Self->Print("<red>\"$ColumnText\" is not a valid Signature name.</red>\n");
+        }
+        return ( 'SignatureID', $SignatureID || 1 );
+
+    } elsif ( $ColumnName =~ m/^follow *up$/i ) {               # Follow Up
+        
+        my $FollowUpID = $Self->{FollowUpOptionsList}->{ $ColumnText };
+        if (!$FollowUpID) {
+            $Self->Print("<red>\"$ColumnText\" is not a valid follow up option name.</red>\n");
+        }
+        return ( 'FollowUpID', $FollowUpID || 1 );
+
+    } elsif ( $ColumnName =~ m/^comment$/i ) {                  # Comment
+
+        return ( 'Comment', $ColumnText || '' );
+
+    } else {
+        $Self->Print("<red>Unknown column name found: \"$ColumnName\".</red>\n");
     }
 
     return;
 }
 
-sub Run {
-    my ( $Self, %Param ) = @_;
+sub ObjectAdd {
+    my ( $Self, %NewObject ) = @_;
 
-    $Self->Print("<yellow>Starting queues import...</yellow>\n");
+    return $Self->{DataObject}->QueueAdd( %NewObject );
+}
 
-    my $LogObject = $Kernel::OM->Get('Kernel::System::Log');
-    my $QueueObject = $Kernel::OM->Get('Kernel::System::Queue');
+sub ObjectUpdate {
+    my ( $Self, %NewObject ) = @_;
 
-    my $CSV = Text::CSV->new(
-        {
-            quote_char  => '"',
-            sep_char    => ',',
-            binary      => 1,
-        }
-    );
-
-    open my $FileHandle, "<:encoding(utf8)", $Self->GetArgument('file') || return $Self->ExitCodeError();
-
-    # Read header line
-    if ( my $HeaderRef = $CSV->getline($FileHandle) ) {
-
-        my %QueueListByName = reverse $QueueObject->QueueList( Valid => 0 );
-
-        QUEUE:
-        # Go through the file, reading each queue definition
-        while ( my $ColRef = $CSV->getline($FileHandle) ) {
-            my @ColumnNames = @{ $HeaderRef };
-            my @Columns = @{ $ColRef };
-            my ($ColumnName, $ColumnText);
-            my %NewQueue = ();
-
-            COLUMN:
-            while ( $ColumnName = shift @ColumnNames ) {
-                $ColumnText = shift @Columns;
-
-                if ( $ColumnName =~ m/^name$/i ) {          # Name
-                    # check if the specified Queue already exists and if yes skip it
-                    next QUEUE if $QueueListByName{$ColumnText};
-
-                    $NewQueue{Name} = $ColumnText;
-                    next COLUMN;
-                } elsif ( $ColumnName =~ m/^valid$/i ) {    # Valid
-                    $NewQueue{Valid} = $ColumnText;
-                    next COLUMN;
-                } elsif ( $ColumnName =~ m/^group$/i ) {    # Group
-                    $NewQueue{Group} = $ColumnText;
-                    next COLUMN;
-                } elsif ( $ColumnName =~ m/^calendar$/i ) { # Calendar
-                    $NewQueue{Calendar} = $ColumnText;
-                    next COLUMN;
-                } elsif ( $ColumnName =~ m/^first.*response.*time$/i ) {    # First Response Time
-                    $NewQueue{FirstResponseTime} = $ColumnText;
-                    next COLUMN;
-                } elsif ( $ColumnName =~ m/^update.*time$/i ) {     # Update Time
-                    $NewQueue{UpdateTime} = $ColumnText;
-                    next COLUMN;
-                } elsif ( $ColumnName =~ m/^solution.*time$/i ) {   # Solution Time
-                    $NewQueue{SolutionTime} = $ColumnText;
-                    next COLUMN;
-                } elsif ( $ColumnName =~ m/^unlock.*timeout$/i ) {  # Unlock Timeout
-                    $NewQueue{UnlockTimeout} = $ColumnText;
-                    next COLUMN;
-                } elsif ( $ColumnName =~ m/^system.*address.*name$/i ) {  # System Address Name
-                    $NewQueue{SystemAddressName} = $ColumnText;
-                    next COLUMN;
-                } elsif ( $ColumnName =~ m/^salutation$/i ) {       # Salutation
-                    $NewQueue{Salutation} = $ColumnText;
-                    next COLUMN;
-                } elsif ( $ColumnName =~ m/^signature$/i ) {        # Signature
-                    $NewQueue{Signature} = $ColumnText;
-                    next COLUMN;
-                } elsif ( $ColumnName =~ m/^comment$/i ) {          # Comment
-                    $NewQueue{Comment} = $ColumnText;
-                    next COLUMN;
-                }
-            }
-
-            # check valid status name
-            my $ValidID = $Kernel::OM->Get('Kernel::System::Valid')->ValidLookup( Valid => $NewQueue{Valid} ); 
-            if ( !$ValidID ) {
-                $Self->PrintError("Unknown validity status name found: \"$NewQueue{Valid}\".\n");
-                return $Self->ExitCodeError();
-            }
-
-            # check group
-            my $GroupID = $Kernel::OM->Get('Kernel::System::Group')->GroupLookup( Group => $NewQueue{Group} );
-            if ( !$GroupID ) {
-                $Self->PrintError("Found no GroupID for $NewQueue{Group}\n");
-                return $Self->ExitCodeError();
-            }
-
-            my $SystemAddressObject = $Kernel::OM->Get('Kernel::System::SystemAddress');
-            my $SystemAddressID;
-
-            # check System Address
-            if ( $NewQueue{SystemAddressName} ) {
-                my %SystemAddressList = $SystemAddressObject->SystemAddressList(
-                    Valid => 1
-                );
-                ADDRESS:
-                for my $ID ( sort keys %SystemAddressList ) {
-                    my %SystemAddressInfo = $SystemAddressObject->SystemAddressGet(
-                        ID => $ID
-                    );
-                    if ( $SystemAddressInfo{Name} eq $NewQueue{SystemAddressName} ) {
-                        $SystemAddressID = $ID;
-                        last ADDRESS;
-                    }
-                }
-                if ( !$SystemAddressID ) {
-                    $Self->PrintError("Address $NewQueue{SystemAddressName} not found\n");
-                    return $Self->ExitCodeError();
-                }
-            }
-
-            # find salutation id
-            my %Salutations = $Kernel::OM->Get('Kernel::System::Salutation')->SalutationList();
-            my ($SalutationID) = grep { $NewQueue{Salutation} eq $Salutations{$_} } sort keys %Salutations;
-
-            # find signature id
-            my %Signatures = $Kernel::OM->Get('Kernel::System::Signature')->SignatureList();
-            my ($SignatureID) = grep { $NewQueue{Signature} eq $Signatures{$_} } sort keys %Signatures;
-
-            # add queue
-            my $Success = $Kernel::OM->Get('Kernel::System::Queue')->QueueAdd(
-                Name              => $NewQueue{Name},
-                ValidID           => $ValidID || 1,
-                GroupID           => $GroupID || 1,
-                Calendar          => $NewQueue{Calendar},
-                FirstResponseTime => $NewQueue{FirstResponseTime},
-                UpdateTime        => $NewQueue{UpdateTime},
-                SolutionTime      => $NewQueue{SolutionTime},
-                UnlockTimeout     => $NewQueue{UnlockTimeout},
-                SystemAddressID   => $SystemAddressID || 1,
-                SalutationID      => $SalutationID || 1,
-                SignatureID       => $SignatureID || 1,
-                Comment           => $NewQueue{Comment},
-                UserID            => 1,
-            );
-
-            # error handling
-            if ( !$Success ) {
-                $Self->PrintError("Can't create queue $NewQueue{Name}.\n");
-                return $Self->ExitCodeError();
-            }
-        }
-    }
-
-    $Self->Print("<green>Done.</green>\n");
-
-    return $Self->ExitCodeOk();
+    return $Self->{DataObject}->QueueUpdate( %NewObject );
 }
 
 1;
